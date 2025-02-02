b8cecc4b075fb5770b1336f8ed94180c7784643e
DenisGord <ltybc22011999d@outlook.com>
2025-02-02 19:23:29 +0600

    Code refactor

--- helpers.js
+++ helpers.js
@@ -1,24 +1,27 @@
 const GigaChat = require('gigachat-node').GigaChat;
+const axios = require('axios');
+const cheerio = require('cheerio');
+const Tesseract = require('tesseract.js');
 
 const analyzeIngredientsOnText = async (ingredients) => {
-    const prompt = `
-    Ты — помощник для анализа косметических средств. Твоя задача — проанализировать состав косметического продукта и выдать результат в следующем формате:
+  const prompt = `
+  Ты — эксперт в области косметологии и химии. Проанализируй состав косметического средства, которое я предоставлю. Состав представлен в виде списка ингредиентов. Твоя задача:
 
-    1. Название продукта:  
+    1. *Название продукта:*  
        - Укажи название продукта, если оно есть во входящем сообщении. Если нет, напиши: "Название продукта не указано."
     
-    2. Описание продукта:  
+    2. *Описание продукта:*  
        - Кратко опиши, для чего предназначен продукт (например, "Энзимная пудра с древесным углём для жирной кожи").
     
-    3. Компоненты:  
+    3. *Компоненты:*  
        - Перечисли все ингредиенты из состава.  
        - Для каждого ингредиента добавь краткое описание его функции (например, "Для глубокого очищения и отшелушивания").  
     
-    4. Плюсы ✅:  
+    4. *Плюсы ✅:*  
        - Перечисли полезные ингредиенты, используя эмоджи зеленой галочки ✅.  
        - Добавь пояснение, почему они полезны (например, "Помогает балансировать микробиом кожи").  
     
-    5. Минусы ❌:  
+    5. *Минусы ❌:*  
        - Перечисли потенциально вредные или раздражающие ингредиенты, используя эмоджи красного крестика ❌.  
        - Добавь пояснение, почему они могут быть вредны (например, "Может вызывать раздражение у чувствительной кожи").  
     
@@ -29,42 +32,110 @@ const analyzeIngredientsOnText = async (ingredients) => {
     
        
     Формат ответа:  
-       - Ответ должен быть структурированным (пометь каждый этап проверки цифрой) и легко читаемым (после каждого ингридиента должен быть перенос строки). Используй маркированные списки (не используй ###)  
+       - Ответ должен быть структурированным (пометь каждый этап проверки цифрой) и легко читаемым (после каждого ингридиента должен быть перенос строки). Используй маркированные списки помечай заголовки жирным текстом оборачивая его в *загловок*, не используй ###
       
   Теперь проанализируй входящее сообщение и выполни задачу ${ingredients}
     `
 
-    const client = new GigaChat(process.env.GIGACHAT_API_KEY);
-    await client.createToken()
+  const client = new GigaChat(process.env.GIGACHAT_API_KEY);
+  await client.createToken()
 
-    const response = await client.completion({
-        model: "GigaChat:latest",
-        messages: [
-            {
-                role: "user",
-                content: prompt
-            }
-        ]
-    })
-    return response.choices[0].message.content
+  const response = await client.completion({
+    model: "GigaChat:latest",
+    messages: [
+      {
+        role: "user",
+        content: prompt
+      }
+    ]
+  })
+  return response.choices[0].message.content
 }
 
 const sendLongMessage = async (text, bot, chatId) => {
-    const length = text.length % 4095 ? text.length / 4095 + 1 : text.length / 4095
+  const length = text.length % 4095 ? text.length / 4095 + 1 : text.length / 4095
 
-    for (let i = 0; i < length; i++) {
-        const sendText = text.slice(i * 4095, i * 4095 + 4095)
-        sendText && await bot.sendMessage(chatId, sendText, { parse_mode: "Markdown" })
-    }
+  for (let i = 0; i < length; i++) {
+    const sendText = text.slice(i * 4095, i * 4095 + 4095)
+    sendText && await bot.sendMessage(chatId, sendText, { parse_mode: "Markdown" })
+  }
 }
 
 const splitMessage = (text, maxLength = 4096) => {
-    const messages = [];
-    while (text.length > 0) {
-        messages.push(text.substring(0, maxLength));
-        text = text.substring(maxLength);
-    }
-    return messages;
+  const messages = [];
+  while (text.length > 0) {
+    messages.push(text.substring(0, maxLength));
+    text = text.substring(maxLength);
+  }
+  return messages;
 };
 
-module.exports = { analyzeIngredientsOnText, sendLongMessage, splitMessage }
+const getProductIngredients = async (url) => {
+  try {
+    // Загружаем HTML-код страницы
+    const { data: html } = await axios.get(url);
+
+    // Парсим HTML с помощью Cheerio
+    const $ = cheerio.load(html);
+
+    // Ищем блок с составом (пример для «Золотое Яблоко»)
+    const ingredients = $('[text="состав"] > div').text() // Уточните селектор для состава
+    const brand = $('h1 > a').text()
+    const name = $('h1 > [itemprop="name"]').text()
+    if (!ingredients) {
+      throw new Error('Состав не найден на странице');
+    }
+
+    return { ingredients, brand, name };
+  } catch (error) {
+    console.error('Ошибка при получении состава:', error);
+    throw error;
+  }
+}
+
+const photoAnalyze = async (msg, bot) => {
+  const { id } = msg.chat;
+  const fileId = msg.photo[msg.photo.length - 1].file_id;
+
+  try {
+    // Скачивание фото
+    const fileLink = await bot.getFileLink(fileId);
+    const imageResponse = await axios.get(fileLink, { responseType: 'arraybuffer' });
+
+    // Распознавание текста с помощью Tesseract
+    const { data: { text } } = await Tesseract.recognize(
+      imageResponse.data, // Буфер изображения
+      'rus+eng', // Языки для распознавания
+    );
+
+    if (!text) {
+      throw new Error('Текст не распознан');
+    }
+
+    // Анализ текста с помощью GigaChat
+    const analysis = await analyzeIngredientsOnText(text);
+    const messages = splitMessage(analysis);
+    for (const message of messages) {
+      await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
+    }
+    // sendLongMessage(analysis, bot, id)
+
+  } catch (error) {
+    console.error('Ошибка:', error);
+    bot.sendMessage(id, 'Произошла ошибка при обработке вашего запроса. Попробуйте ещё раз.');
+  }
+}
+
+const textAnalyze = async (text, bot, id, name) => {
+  try {
+    const analysis = await analyzeIngredientsOnText(`название ${name} ингридиенты ${text}`);
+    const messages = splitMessage(analysis);
+    for (const message of messages) {
+      await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
+    }
+  } catch (error) {
+    bot.sendMessage(id, 'Произошла ошибка при анализе. Попробуйте позже.');
+  }
+}
+
+module.exports = { analyzeIngredientsOnText, sendLongMessage, splitMessage, getProductIngredients, photoAnalyze, textAnalyze }
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
-const { analyzeIngredientsOnText, splitMessage } = require('./helpers')
-const Tesseract = require('tesseract.js');
-const axios = require('axios');
-const cheerio = require('cheerio');
+const { getProductIngredients, photoAnalyze, textAnalyze } = require('./helpers')
+
 require('dotenv').config()
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
@@ -10,27 +8,20 @@ const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 //Обрбаботчик команд
 bot.onText(/\/start/, (msg) => {
   const { id } = msg.chat
-  bot.sendMessage(id, `Привет ${msg.from.first_name}! Пришли мне список ингредиентов косметического средства или фотогравию состава, и я их проанализирую.`, {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: "текст 💬",
-            callback_data: 'text'
-          },
-          {
-            text: "фото 📸",
-            callback_data: 'img'
-          }
-        ]
-      ]
-    }
-  })
+  bot.sendMessage(id, `🌟 *${msg.from.first_name} добро пожаловать в бот для анализа косметики! *🌟
+  
+__Ты можешь прислать мне:__
+
+🔗 ссылку на продукт из золотого яблока
+
+📝 список ингредиентов косметического средства 
+
+📸 фотогравию состава, и я их проанализирую.`, { parse_mode: 'Markdown' })
 })
 
 bot.onText(/\/analyze_text/, (msg) => {
   const { id } = msg.chat
-  bot.sendMessage(id, `Пришли мне список ингредиентов косметического средства и я их проанализирую.`);
+  bot.sendMessage(id, `Пришли мне ссылку список ингредиентов косметического средства и я их проанализирую.`);
 })
 
 bot.onText(/\/analyze_photo/, (msg) => {
@@ -38,129 +29,29 @@ bot.onText(/\/analyze_photo/, (msg) => {
   bot.sendMessage(id, `Пришли мне фотографию состава косметического средства и я их проанализирую.`);
 })
 
-bot.on('photo', async (msg) => {
-  const { id } = msg.chat;
-  const fileId = msg.photo[msg.photo.length - 1].file_id;
-
-  try {
-    bot.sendMessage(id, "Выполняем анализ вашего средства ✅", { parse_mode: 'Markdown' })
-
-    // Скачивание фото
-    const fileLink = await bot.getFileLink(fileId);
-    const imageResponse = await axios.get(fileLink, { responseType: 'arraybuffer' });
-
-    // Распознавание текста с помощью Tesseract
-    const { data: { text } } = await Tesseract.recognize(
-      imageResponse.data, // Буфер изображения
-      'rus+eng', // Языки для распознавания
-    );
-
-    if (!text) {
-      throw new Error('Текст не распознан');
-    }
-
-    // Анализ текста с помощью GigaChat
-    const analysis = await analyzeIngredientsOnText(text);
-    const messages = splitMessage(analysis);
-    for (const message of messages) {
-      await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
-    }
-    // sendLongMessage(analysis, bot, id)
-
-  } catch (error) {
-    console.error('Ошибка:', error);
-    bot.sendMessage(id, 'Произошла ошибка при обработке вашего запроса. Попробуйте ещё раз.');
-  }
-});
-
-//Обработчик квери команд
-
-bot.on('callback_query', query => {
-  const { id } = query.message.chat
-
-  switch (query.data) {
-    case 'text':
-      break;
-    case "img":
-      break;
-  }
-})
-
 // Обработчик текстовых сообщений
 bot.on('message', async (msg) => {
   const { id } = msg.chat;
   const text = msg.text;
-  //костыль чтобы не реагировать на команды
-  if (text && text?.[0] !== '/') {
+  const photo = msg.photo
+  if (text && text[0] !== '/') {
     bot.sendMessage(id, "Выполняем анализ вашего средства ✅", { parse_mode: 'Markdown' })
 
-    if (text.startsWith('https://goldapple.ru')) {
-      const ingredients = await getProductIngredients(text);
-      try {
-        const analysis = await analyzeIngredientsOnText(ingredients);
-        const messages = splitMessage(analysis);
-        for (const message of messages) {
-          await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
-        }
-      } catch (error) {
-        console.error('Ошибка:', error);
-        bot.sendMessage(id, 'Произошла ошибка при анализе. Попробуйте позже.');
-      }
-    }
-    try {
-      const analysis = await analyzeIngredientsOnText(text);
-      const messages = splitMessage(analysis);
-      for (const message of messages) {
-        await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
-      }
-    } catch (error) {
-      console.error('Ошибка:', error);
-      bot.sendMessage(id, 'Произошла ошибка при анализе. Попробуйте позже.');
+    switch (true) {
+      case Boolean(photo):
+        photoAnalyze(msg, bot)
+        break;
+      case text.startsWith('https://goldapple.ru'):
+        const { ingredients, brand, name } = await getProductIngredients(text)
+        // const price = await getProductIngredients(text, '[itemprop="priceSpecification"] > div')
+        textAnalyze(ingredients, bot, id, brand + name)
+        break;
+      case text && text?.[0] !== '/':
+        textAnalyze(text, bot, id)
+        break;
     }
   }
-
 });
-
-const getProductIngredients = async (url) => {
-  try {
-    // Загружаем HTML-код страницы
-    const { data: html } = await axios.get(url);
-
-    // Парсим HTML с помощью Cheerio
-    const $ = cheerio.load(html);
-
-    // Ищем блок с составом (пример для «Золотое Яблоко»)
-    const ingredients = $('[text="состав"] > div').text() // Уточните селектор для состава
- 
-    if (!ingredients) {
-      throw new Error('Состав не найден на странице');
-    }
-
-    return ingredients;
-  } catch (error) {
-    console.error('Ошибка при получении состава:', error);
-    throw error;
-  }
-};
-
-// async function addImg(file) {
-//   const client = new GigaChat(GIGACHAT_API_KEY);
-//   await client.createToken()
-
-//   const response = await client.completion({
-//     model: "GigaChat:latest",
-//     messages: [
-//       {
-//         role: "user",
-//         content: 'определи состав косметологического средства на изображении и опиши ингридиенты',
-//         file,
-//         image: file
-//       }
-//     ]
-//   })
-
-//   console.log(response.choices[0].message.content, 'content')
-// }
 
 
 console.log('Бот запущен...');
